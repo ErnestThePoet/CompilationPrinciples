3ac35d711f8ff66a80de1f35521188cc76dcb5f7
Ernest Cui <ecuiships@126.com>
2023-03-25 14:43:04 +0800

    mkdir only if not exists

--- Lab1/build.sh
+++ Lab1/build.sh
@@ -1,6 +1,6 @@
-mkdir build
+mkdir -p build
 GENERATED_DIR=generated
-mkdir $GENERATED_DIR
+mkdir -p $GENERATED_DIR
 flex -o $GENERATED_DIR/lex_analyser.c --header-file=$GENERATED_DIR/lex_analyser.h lex_analyser.l \
 && bison -o $GENERATED_DIR/parser.c -H$GENERATED_DIR/parser.h parser.y \
 && cmake . -Bbuild \
--- Lab1/parse.sh
+++ Lab1/parse.sh
@@ -1,3 +1,3 @@
-mkdir out
+mkdir -p out
 file_name=$(basename $1)
 ./build/parser $1 > ./out/${file_name}_out.txt 2>&1
