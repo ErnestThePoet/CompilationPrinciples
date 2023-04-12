6bead7c6ad389392ff7e567a4016b1b3e980391e
Ernest Cui <ecuiships@126.com>
2023-04-12 21:26:30 +0800

    created ir_generator

---
+++ Lab3/bits/ir_generator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../../Lab2/bits/semantic_analyser.h"
+
+class IrGenerator{
+    private:
+        SymbolTable symbol_table_;
+        StructDefSymbolTable struct_def_symbol_table_;
+};
