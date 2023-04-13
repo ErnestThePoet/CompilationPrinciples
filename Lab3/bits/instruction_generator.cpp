fd770385eda0760d67bc11025ef301c2754c3fb4
Ernest Cui <ecuiships@126.com>
2023-04-13 20:12:50 +0800

    make GenerateImm public and modified some arg name

--- Lab3/bits/instruction_generator.cpp
+++ Lab3/bits/instruction_generator.cpp
@@ -11,11 +11,6 @@ const std::string InstructionGenerator::kBinaryOperatorLe = "<=";
 const std::string InstructionGenerator::kBinaryOperatorEq = "==";
 const std::string InstructionGenerator::kBinaryOperatorNe = "!=";
 
-std::string InstructionGenerator::ToImm(const std::string &number) const
-{
-    return "#" + number;
-}
-
 std::string InstructionGenerator::GenerateLabel(const std::string &name) const
 {
     return "LABEL " + name + " :";
@@ -26,24 +21,22 @@ std::string InstructionGenerator::GenerateFunction(const std::string &name) cons
     return "FUNCTION " + name + " :";
 }
 
+std::string InstructionGenerator::GenerateImm(const std::string &number) const
+{
+    return "#" + number;
+}
+
 std::string InstructionGenerator::GenerateAssign(const std::string &left,
-                                                 const std::string &right,
-                                                 const bool is_imm = false) const
+                                                 const std::string &right) const
 {
     return left + " := " + right;
 }
 
 std::string InstructionGenerator::GenerateBinaryOperation(const std::string binary_operator,
                                                           const std::string &left,
-                                                          const std::string &right,
-                                                          const bool is_left_imm = false,
-                                                          const bool is_right_imm = false) const
+                                                          const std::string &right) const
 {
-    return (is_left_imm ? ToImm(left) : left) +
-           ' ' +
-           binary_operator +
-           ' ' +
-           (is_right_imm ? ToImm(right) : right);
+    return left + ' ' + binary_operator + ' ' + right;
 }
 
 std::string InstructionGenerator::GenerateAddress(const std::string &variable) const
@@ -67,9 +60,9 @@ std::string InstructionGenerator::GenerateIf(const std::string &condition,
     return "IF " + condition + ' ' + GenerateGoto(goto_label_name);
 }
 
-std::string InstructionGenerator::GenerateReturn(const std::string &variable) const
+std::string InstructionGenerator::GenerateReturn(const std::string &value) const
 {
-    return "RETURN " + variable;
+    return "RETURN " + value;
 }
 
 std::string InstructionGenerator::GenerateDec(const std::string &variable, const size_t size) const
@@ -77,9 +70,9 @@ std::string InstructionGenerator::GenerateDec(const std::string &variable, const
     return "DEC " + variable + " [" + std::to_string(size) + ']';
 }
 
-std::string InstructionGenerator::GenerateArg(const std::string &variable) const
+std::string InstructionGenerator::GenerateArg(const std::string &value) const
 {
-    return "ARG " + variable;
+    return "ARG " + value;
 }
 
 std::string InstructionGenerator::GenerateCall(const std::string &variable) const
@@ -97,7 +90,7 @@ std::string InstructionGenerator::GenerateRead(const std::string &variable) cons
     return "READ " + variable;
 }
 
-std::string InstructionGenerator::GenerateWrite(const std::string &variable) const
+std::string InstructionGenerator::GenerateWrite(const std::string &value) const
 {
-    return "WRITE " + variable;
+    return "WRITE " + value;
 }
--- Lab3/bits/instruction_generator.h
+++ Lab3/bits/instruction_generator.h
@@ -4,9 +4,6 @@
 
 class InstructionGenerator
 {
-private:
-    std::string ToImm(const std::string &number) const;
-
 public:
     const static std::string kBinaryOperatorAdd;
     const static std::string kBinaryOperatorSub;
@@ -19,26 +16,27 @@ public:
     const static std::string kBinaryOperatorEq;
     const static std::string kBinaryOperatorNe;
 
+    // "number" argument should contain a number literal.
+    // "variable" argument should contain a variable name.
+    // "value" argument should contain either a number literal or variable name.
     std::string GenerateLabel(const std::string &name) const;
     std::string GenerateFunction(const std::string &name) const;
+    std::string GenerateImm(const std::string &number) const;
     std::string GenerateAssign(const std::string &left,
-                               const std::string &right,
-                               const bool is_imm = false) const;
+                               const std::string &right) const;
     std::string GenerateBinaryOperation(const std::string binary_operator,
                                         const std::string &left,
-                                        const std::string &right,
-                                        const bool is_left_imm = false,
-                                        const bool is_right_imm = false) const;
+                                        const std::string &right) const;
     std::string GenerateAddress(const std::string &variable) const;
     std::string GenerateDereference(const std::string &variable) const;
     std::string GenerateGoto(const std::string &label_name) const;
     std::string GenerateIf(const std::string &condition,
                            const std::string goto_label_name) const;
-    std::string GenerateReturn(const std::string &variable) const;
+    std::string GenerateReturn(const std::string &value) const;
     std::string GenerateDec(const std::string &variable, const size_t size) const;
-    std::string GenerateArg(const std::string &variable) const;
+    std::string GenerateArg(const std::string &value) const;
     std::string GenerateCall(const std::string &variable) const;
     std::string GenerateParam(const std::string &variable) const;
     std::string GenerateRead(const std::string &variable) const;
-    std::string GenerateWrite(const std::string &variable) const;
+    std::string GenerateWrite(const std::string &value) const;
 };
